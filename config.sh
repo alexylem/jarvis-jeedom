92a85b817627383ef0eb4ea4ec257e7bf1888742
Alexandre Mély <alexandre.mely@gmail.com>
2016-11-11 13:28:16 +0100

    added optional path variable if jeedom not at root

--- config.sh
+++ config.sh
@@ -1,3 +1,4 @@
 jv_pg_jd_ip="<JEEDOM_IP_ADDRESS>" # ex: "192.168.1.1"
 jv_pg_jd_port=80 # default is 80
+jv_pg_jd_path="" # default is "". Set for ex: "jeedom/" if your jeedom is not at root
 jv_pg_jd_apikey="<JEEDOM_API_KEY>" # Find it in Jeedom / Géneral / Administration / Configuration
